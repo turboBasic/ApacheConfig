cf5e8e17868e7b0bb3a53bc31f43959a1a00140f
turboBasic <off@boun.cr>
2018-10-20 03:10:53 +0300

    Improve: env_vars; rsync

--- install.sh
+++ install.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 APACHE=/etc/apache2
-WWW=/var/www/html
+WWW_BASE=${WWW_BASE:-"/var/www"}
 
-sudo cp apache2.conf ${APACHE}/ --verbose
-sudo cp ports.conf ${APACHE}/   --verbose
-sudo cp sites-available/* ${APACHE}/sites-available --verbose
-#sudo cp html/magento ${WWW}/    --verbose
+declare -a files=(
+    "apache2.conf"
+    "ports.conf"
+    "sites-available/*"
+    "envvars"
+)
+
+#for i in ${files[@]}
+#do
+#    if [[ -f ${APACHE}/${i} ]]
+#    then
+#        sudo  mv --verbose ${APACHE}/${i} ${APACHE}/${i}.$(date +%Y%m%d-%H%M%s).bak
+#    fi
+#    sudo cp --recursive --verbose ${i} ${APACHE}/${i}
+#done
+
+#sudo cp --verbose html/magento      ${WWW_BASE}/
+
+
+sudo rsync --recursive --perms --verbose ./etc_apache2/ /etc/apache2/
 
 sudo systemctl restart apache2
 
